f74cefaeb805f585c9b37ff16e5dbafc2c1d0385
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-06 17:44:39 -0600

    refactor: 💡 Formatting utils folder

--- lib/Utils/Toast.dart
+++ lib/Utils/Toast.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
-void showToast(BuildContext context, String text, {
-    String buttonLabel = 'Ok',
+void showToast(BuildContext context, String text,
+    {String buttonLabel = 'Ok',
     Duration duration = const Duration(seconds: 3),
     DismissDirection dismissDirection = DismissDirection.down,
-    ToastStatus toastStatus = ToastStatus.none
-  }) {
+    ToastStatus toastStatus = ToastStatus.none}) {
   final scaffold = ScaffoldMessenger.of(context);
   Color? textColor = getTextColor(toastStatus);
   scaffold.showSnackBar(
     SnackBar(
       content: Text(text),
       action: SnackBarAction(
-        label: buttonLabel,
-        onPressed: scaffold.hideCurrentSnackBar,
-        textColor: textColor
-      ),
+          label: buttonLabel,
+          onPressed: scaffold.hideCurrentSnackBar,
+          textColor: textColor),
       duration: duration,
       dismissDirection: dismissDirection,
     ),
@@ -23,7 +21,7 @@ void showToast(BuildContext context, String text, {
 }
 
 Color? getTextColor(ToastStatus toastStatus) {
-  switch(toastStatus) {
+  switch (toastStatus) {
     case ToastStatus.success:
       return Colors.greenAccent;
     case ToastStatus.warning:
@@ -37,10 +35,4 @@ Color? getTextColor(ToastStatus toastStatus) {
   }
 }
 
-enum ToastStatus {
-  none,
-  info,
-  warning,
-  error,
-  success
-}
+enum ToastStatus { none, info, warning, error, success }
